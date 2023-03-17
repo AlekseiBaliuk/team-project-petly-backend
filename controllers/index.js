b29e78635ece3492ce46148629ae85dc40bbad25
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2023-03-17 18:50:43 +0200

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const auth = require("./auth");
 // const friends = require("./friends");
 // const news = require("./news");
 // const notices = require("./notices");
-// const users = require("./users");
-// const userPets = require("./userPets");
+const users = require("./users");
+const userPets = require("./userPets");
 
-module.exports = { auth };
+module.exports = { auth, users, userPets };
