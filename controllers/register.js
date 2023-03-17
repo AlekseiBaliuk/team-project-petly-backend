ad89d22f804c4c304e0065f9965a16d8441f2f68
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2023-03-17 12:03:59 +0200

    create user model + registration

---
+++ controllers/register.js
@@ -0,0 +1,44 @@
+const { User } = require("../models/user");
+const HttpErrors = require("../helpers/HttpError");
+const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
+// const { sendEmail } = require("../../helpers");
+const { v4 } = require("uuid");
+
+const register = async (req, res) => {
+  const { password, email, name, city, phone } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpErrors(409);
+  }
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const avatarURL = gravatar.url(email);
+  // const verificationEmailToken = v4();
+  const result = await User.create({
+    password: hashPassword,
+    email,
+    name,
+    city,
+    phone,
+    avatarURL,
+  });
+  // const mail = {
+  //   to: email,
+  //   subject: " Registration confirmation",
+  //   html: `<a href="http://localhost:3000/api/auth/verify/${verificationEmailToken}" target="_blank">Click for email confirmation</a>`,
+  // };
+  //   await sendEmail(mail);
+  //   res.status(201).json({
+  //     status: "success",
+  //     code: 201,
+  //     data: {
+  //       user: {
+  //         email,
+  //         subscription,
+  //         avatarURL,
+  //       },
+  //     },
+  //   });
+};
+
+module.exports = register;
