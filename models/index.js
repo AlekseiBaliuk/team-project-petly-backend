1e9fd8485aecbef5fac915eff990227fba995342
Aleksei Baliuk <aleksred.b@gmail.com>
2023-03-20 09:26:35 +0200

    fix

--- models/index.js
+++ models/index.js
@@ -3,6 +3,6 @@ const { User } = require("./user");
 const { UserPet } = require("./userPets");
 const { userPetSchema } = require("./userPets");
 const { Notice } = require("./notice");
-const { News } = require("./News");
+const { News } = require("./news");
 
 module.exports = { User, UserPet, userPetSchema, Notice, News, Friend };
