60cee7455cb160d34ae8c346f047c266987061d4
kobaserg <kobasts@gmail.com>
2023-03-21 16:16:15 +0100

    Update isValidPetId.js

--- middlewares/isValidPetId.js
+++ middlewares/isValidPetId.js
@@ -2,7 +2,7 @@ const { isValidObjectId } = require("mongoose");
 const { HttpError } = require("../helpers");
 
 const isValidPetId = (req, res, next) => {
-  const { id: petId } = req.params;
+  const { petId } = req.params;
   if (!isValidObjectId(petId)) {
     next(HttpError(404, "Invalid petId"));
   }
